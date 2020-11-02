01c6a77f99dd1dcf38796dbc568191ae89d7b283
TE-KosukeMatsusaki <kosuke.matsusaki@sharing-innovations.co.jp>
2020-11-02 18:23:54 +0900

    feat: add new project

---
+++ sample_storyboard_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample_storyboard_app
+//
+//  Created by Kosuke Matsusaki on 2020/10/30.
+//  Copyright © 2020 Kosuke Matsusaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
